1b09b3b16673baee74b83052b3b39a6a426d4222
shinheekim <dev.shinheekim@gmail.com>
2025-09-06 17:31:33 +0900

    ✨ [#10] 주문 잠김 API 구현

--- src/main/java/com/example/demo/stock/repository/OrdersRepository.java
+++ src/main/java/com/example/demo/stock/repository/OrdersRepository.java
@@ -2,6 +2,16 @@ package com.example.demo.stock.repository;
 
 import com.example.demo.stock.entity.Orders;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface OrdersRepository extends JpaRepository<Orders, Long> {
+    
+    @Query("SELECT o FROM Orders o WHERE o.round.id = :roundId AND o.team.id = :teamId")
+    List<Orders> findByRoundIdAndTeamId(@Param("roundId") Long roundId, @Param("teamId") Long teamId);
+    
+    @Query("SELECT o FROM Orders o WHERE o.round.id = :roundId")
+    List<Orders> findByRoundId(@Param("roundId") Long roundId);
 }
